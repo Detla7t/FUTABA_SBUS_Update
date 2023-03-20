5bdc147e5d1534d1e711875087bbba383453bf93
Detla7t <103520358+Detla7t@users.noreply.github.com>
2023-03-19 23:29:14 -0400

    Update sbus_example.ino

--- FUTABA_SBUS/examples/sbus_example/sbus_example.ino
+++ FUTABA_SBUS/examples/sbus_example/sbus_example.ino
@@ -1,21 +1,63 @@
+#include <Arduino.h>
 #include <FUTABA_SBUS.h>
-#include <Streaming.h>
+#include <Streaming.h> //not sure if this is needed dont have an easy way to check i would recommend trying to remove this line and see if you have issues or not
 
 
-FUTABA_SBUS sBus;
-
-
+FUTABA_SBUS sBus(Serial1);
+int16_t* CH = sBus.UpdateChannels(); //allows you to easily parse the Channels by doing CH[Channel number] to get the value of any specific channel 
+How_many_cycles_before_you_update_the_receiver_values = 3500 //adjust how fast it updates the values received NOTE IF YOU HAVE ISSUES WITH NO VALUES INCREASE THIS OTHERWISE IF YOU HAVE ISSUES WITH VALUES BEING STUCK TRY DECREASING THIS VALUE
+How_many_cycles_before_checking_the_receiver_after_an_error = How_many_cycles_before_you_update_the_receiver_values * 10 //
+  
 void setup(){
   sBus.begin();
-  Serial.begin(115200);
+  Serial.begin(9600);
 }
 
 void loop(){
-  sBus.FeedLine();
-  if (sBus.toChannels == 1){
-    sBus.UpdateServos();
-    sBus.UpdateChannels();
-    sBus.toChannels = 0;
-    Serial<<sBus.channels[0]<<","<<sBus.channels[1]<<","<<sBus.channels[2]<<"\r\n";
+  if(RecieveCheck()) { //if receiver is working normally
+    //do something
   }
+}
+
+Generic_Wait(int number,int number_limit) {
+    if (number >= number_limit) {
+        number = 0;
+    }
+    number++;
+    return number;
+}
+
+int Receiver_Cycle_count = 0;
+int Error_Cycle_count = 0;
+
+bool RecieveCheck() {
+    Receiver_Cycle_count = Generic_Wait(Receiver_Cycle_count,How_many_cycles_before_you_update_the_receiver_values);
+    Error_Cycle_count = Generic_Wait(Error_Cycle_count,How_many_cycles_before_checking_the_receiver_after_an_error);
+    if (CH[0] == 0) {
+        if (Error_Cycle_count >= 35000) {
+            Serial.print("Error_0x500\n"); //Failed to get Signal Update Retrying
+            sBus.ReadDevice();
+        }
+        return false;
+    } else {
+        if (Receiver_Cycle_count >= 3500) {
+            sBus.ReadDevice();
+            Serial.print(CH[1]);
+            Serial.print("\t");
+            Serial.print(CH[2]);
+            Serial.print("\t");
+            Serial.print(CH[3]);
+            Serial.print("\t");
+            Serial.print(CH[4]);
+            Serial.print("\t");
+            Serial.print(CH[5]);
+            Serial.print("\t");
+            Serial.print(CH[6]);
+            Serial.print("\t");
+            Serial.print(CH[7]);
+            Serial.print("\t");
+            Serial.println(CH[8]);
+        }
+        return true;
+    }
 }
